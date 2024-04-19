76b957525e868a9163e0293c66f29e1d186a31a9
Ben Pratt <benjaminrpratt@gmail.com>
2024-04-19 02:53:20 +0000

    address nullability warnings

--- DiceRollOperator/DiceRoll.cs
+++ DiceRollOperator/DiceRoll.cs
@@ -9,13 +9,13 @@ namespace DiceRollOperator;
 public class DiceRollSpec
 {
     [JsonPropertyName("dice")]
-    public string[] Dice { get; set; }
+    public string[] Dice { get; set; } = [];
 }
 
 public class DiceRollResult
 {
     [JsonPropertyName("die")]
-    public string Die { get; set; }
+    public required string Die { get; set; }
 
     [JsonPropertyName("value")]
     public int Value { get; set; }
@@ -27,25 +27,25 @@ public class DiceRollStatus
     public int Total { get; set; }
 
     [JsonPropertyName("results")]
-    public List<DiceRollResult> Results { get; set; }
+    public List<DiceRollResult> Results { get; set; } = [];
 }
 
 public class DiceRoll : IKubernetesObject<V1ObjectMeta>
 {
     [JsonPropertyName("metadata")]
-    public V1ObjectMeta Metadata { get; set; }
+    public required V1ObjectMeta Metadata { get; set; }
 
     [JsonPropertyName("apiVersion")]
-    public string ApiVersion { get; set; }
+    public required string ApiVersion { get; set; }
 
     [JsonPropertyName("kind")]
-    public string Kind { get; set; }
+    public required string Kind { get; set; }
 
     [JsonPropertyName("spec")]
-    public DiceRollSpec Spec { get; set; }
+    public required DiceRollSpec Spec { get; set; }
 
     [JsonPropertyName("status")]
-    public DiceRollStatus Status { get; set; }
+    public DiceRollStatus? Status { get; set; }
 }
 
 // use this instead?
@@ -53,13 +53,13 @@ public class DiceRoll : IKubernetesObject<V1ObjectMeta>
 public class DiceRollList : IKubernetesObject<V1ListMeta>
 {
     [JsonPropertyName("metadata")]
-    public V1ListMeta Metadata { get; set; }
+    public required V1ListMeta Metadata { get; set; }
 
     [JsonPropertyName("apiVersion")]
-    public string ApiVersion { get; set; }
+    public required string ApiVersion { get; set; }
 
     [JsonPropertyName("kind")]
-    public string Kind { get; set; }
+    public required string Kind { get; set; }
 
-    public List<DiceRoll> Items { get; set; }
+    public List<DiceRoll> Items { get; set; } = [];
 }
